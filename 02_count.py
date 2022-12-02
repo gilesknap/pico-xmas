715df38588439acc6f8468678677f7e9bbc8b49a
Giles Knap <gilesknap@gmail.com>
2022-12-02 22:42:35 +0000

    fixed red/green wiring

--- 02_count.py
+++ 02_count.py
@@ -7,22 +7,24 @@ counter = 1  # Set the counter to 1
 
 
 # assign LEDs to binary digits LSB to MSB
-led_bits = [red, amber, green, onboardLED]
+led_bits = [green, amber, red, onboardLED]
 
-while True:  # Keep on counting forever
 
-    # count from 0 to 15 (2^4 values including 0, using 4 LEDs)
-    for num in range(16):
-        # convert to binary string but remove '0b' from the beginning
-        binary = bin(num)[2:]
-        # pad the left of the binary value with zeros
-        binary = "%04d" % int(binary)
-        # print the number and it's 4 digit binary equivalent
-        print(f"{num} = binary {binary}")
+def count_led(pause=0.5):
+    while True:  # Keep on counting forever
 
-        # loop over the 4 LED bits
-        for bit, led_bit in enumerate(led_bits):
-            # set the LED according to its equivalent position in the binary string
-            led_bits[bit].value(int(binary[bit]))
+        # count from 0 to 15 (2^4 values including 0, using 4 LEDs)
+        for num in range(16):
+            # convert to binary string but remove '0b' from the beginning
+            binary = bin(num)[2:]
+            # pad the left of the binary value with zeros
+            binary = "%04d" % int(binary)
+            # print the number and it's 4 digit binary equivalent
+            print(f"{num} = binary {binary}")
 
-        sleep(0.5)
+            # loop over the 4 LED bits
+            for bit, led_bit in enumerate(led_bits):
+                # set the LED according to its equivalent position in the binary string
+                led_bits[bit].value(int(binary[bit]))
+
+            sleep(pause)
