617be0286270f22b61ba95701553aa9afbdb09ae
Giles Knap <gilesknap@gmail.com>
2023-12-05 13:03:45 +0000

    update README

--- e03_buttons.py
+++ e03_buttons.py
@@ -4,6 +4,10 @@ from hardware.async2023 import green_button, onboard_led, red_button, red_led
 
 
 class DoBlink:
+    """
+    A class to blink an LED in the background using asyncio.
+    """
+
     def __init__(self, led, period_ms):
         self.running = False
         self.led = led
@@ -31,6 +35,8 @@ blink1 = DoBlink(onboard_led, 700)
 blink2 = DoBlink(red_led, 400)
 
 
+# callback function for red button, toggles modes when it is pressed
+# and does nothing when it is released
 def switch_mode(button):
     global MODE
     if button.pressed:
@@ -43,6 +49,8 @@ def switch_mode(button):
         change_leds()
 
 
+# a callback function for the green button, toggles the ENABLED flag when
+# it is pressed and does nothing when it is released
 def switch_enabled(button):
     if button.pressed:
         # don't switch enabled in here to demo use of asyncio Button functions
@@ -50,6 +58,8 @@ def switch_enabled(button):
         print("toggle enable LEDs")
 
 
+# a function for switching the LEDs state. Call this when mode or enabled
+# flags have changed
 def change_leds():
     blink1.stop()
     blink2.stop()
@@ -64,6 +74,11 @@ def change_leds():
         red_led.off()
 
 
+# main function must be async to use asyncio features.
+# this just sets up the buttons and monitors the green button for presses
+# to toggle the ENABLED flag. The red button is monitored in the background
+# by the Button class and calls the switch_mode callback function when it
+# is pressed.
 async def main():
     global ENABLED
     red_button(callback=switch_mode)
@@ -76,4 +91,5 @@ async def main():
         await go_button.wait_for_release()
 
 
+# launch the main function asynchronously
 asyncio.run(main())
