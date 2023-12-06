cfd3fd69d61c561ca50fbec6dd17030d2b3a910b
Giles Knap <gilesknap@gmail.com>
2023-12-06 20:39:10 +0000

    improve layout of day 4 main

--- e04_segmented.py
+++ e04_segmented.py
@@ -2,10 +2,15 @@ import asyncio
 
 from asyn.segmented import Segmented
 
-# create the Segmented display object
-s = Segmented()
-# start the background task that will count up in binary on the display
-s.start_count()
 
-# just wait for a long time and let the background task do its thing
-asyncio.run(s._counter(500000))
+async def main():
+    # create the Segmented display object
+    s = Segmented()
+    # start the background task that will count up in binary on the display
+    s.start_count()
+
+    # just wait for a long time and let the background task do its thing
+    await asyncio.sleep(60000)
+
+
+asyncio.run(main())
